2ad47f74388a4bb186c2c738b768ca8270b65f97
tomvmot <78728126+tomvmot@users.noreply.github.com>
2021-02-10 11:48:44 +0100

    comment: test

---
+++ PROG_ultrasoon/PROG_ultrasoon.ino
@@ -0,0 +1,44 @@
+#include <RTClib.h>
+//test
+const int trigPin = 4;
+const int echoPin= 16;
+
+long duration;
+double distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+//RTC_DS1307 rtc;
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin,LOW);
+
+duration = pulseIn(echoPin,HIGH);
+distance = duration*0.034/2;
+
+Serial.println(""+String(distance) + "          ");
+//Serial.println(duration);
+delay(10);
+
+}
+/*
+double time(){
+DateTime now = rtc.now();
+tijdNu = now.
+}
+
+}
+*/

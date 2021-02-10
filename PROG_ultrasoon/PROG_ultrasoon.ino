e5f60bef282cbc2e4a01966f0f8adc18f2f3f44f
TonySalmonelli <78727734+TonySalmonelli@users.noreply.github.com>
2021-02-10 18:00:35 +0100

    update voor 4 sensoren

--- PROG_ultrasoon/PROG_ultrasoon.ino
+++ PROG_ultrasoon/PROG_ultrasoon.ino
@@ -1,15 +1,15 @@
 #include <RTClib.h>
 //test
-const int trigPin = 4;
-const int echoPin= 16;
-
-long duration;
-double distance;
+const int trigPin[] = {4,12,18,2};
+const int echoPin[] = {16,14,19,15};
+double distance[4];
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);
+  for(int t=0;t<4;t++){
+    pinMode(trigPin[t],OUTPUT);
+    pinMode(echoPin[t],INPUT);
+  }
   Serial.begin(9600);
   
 
@@ -17,27 +17,37 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sonicsensor();
+  Serial.print(distance[0]);
+  Serial.print("\t");
+  Serial.print(distance[1]);
+  Serial.print("\t");
+  Serial.print(distance[2]);
+  Serial.print("\t");
+  Serial.println(distance[3]);
+    //Serial.println(duration);
 
-//RTC_DS1307 rtc;
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
+}
+void sonicsensor(){
+  long duration;
+  for(int t=0;t<4;t++){
+    digitalWrite(trigPin[t], LOW);
+    delayMicroseconds(2);
 
-digitalWrite(trigPin,HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin,LOW);
+    digitalWrite(trigPin[t],HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin[t],LOW);
 
-duration = pulseIn(echoPin,HIGH);
-distance = duration*0.034/2;
-
-Serial.println(""+String(distance) + "          ");
-//Serial.println(duration);
-delay(10);
-
+    duration = pulseIn(echoPin[t],HIGH);
+    distance[t] = duration*0.034/2;
+    delay(200);
+  }
 }
 /*
 double time(){
 DateTime now = rtc.now();
-tijdNu = now.
+tijdNu = no
+w.
 }
 
 }
